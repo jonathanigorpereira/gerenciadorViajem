d743d74a386dcf37e25abd27fca5836c84a7b8ec
Jonathan Bockorny <jonathan.igor.pereira@hotmail.com>
2024-10-20 22:18:41 -0300

    novo front

---
+++ frontend/src/Components/Travels/Destination.jsx
@@ -0,0 +1,197 @@
+import { IconBed, IconCar, IconPizza, IconPlaneArrival, IconTicket, IconTrash } from "@tabler/icons-react";
+import { useEffect, useState } from "react";
+import { GetFederativeUnits } from "../../Services/FederativeUnits";
+import { GetCities } from "../../Services/Cities";
+import { DateInput } from "../Forms/DateInput";
+
+export default function TravelDestination({ index, setTravel, updateDestination, updateCost, removeDestination, ...destino }) {
+
+    const [federativeUnits, setFederativeUnits] = useState([]);
+    const [cities, setCities] = useState([]);
+
+    const [importingFederativeUnits, setImportingFederativeUnits] = useState(false);
+    const [importingCities, setImportingCities] = useState(false);
+
+    useEffect(() => { getFederativeUnits() }, []);
+
+    useEffect(() => { if (destino.idUnidadeFederativa) { getCities() } }, [destino.idUnidadeFederativa]);
+
+    async function getFederativeUnits() {
+        setImportingFederativeUnits(true);
+        try {
+            const result = await GetFederativeUnits();
+            setFederativeUnits(result);
+        }
+        catch (error) {
+
+        }
+        setImportingFederativeUnits(false);
+    }
+
+    async function getCities() {
+        setImportingCities(true);
+        try {
+            const result = await GetCities(destino.idUnidadeFederativa);
+            setCities(result);
+        }
+        catch (error) {
+
+        }
+        setImportingCities(false);
+    }
+
+    return (
+        <div className="card border-success">
+            <div className="card-header">
+                <div className="row align-items-center">
+                    <div className="col">
+                        <h6 className="card-title text-uppercase mb-0">
+                            <IconPlaneArrival className="me-2" size={36} stroke={1} />
+                            Destino {index + 1}
+                        </h6>
+                    </div>
+                    <div className="col">
+                        <button type="button" className="btn btn-sm float-end px-2" onClick={() => removeDestination(index)}>
+                            <IconTrash className="icon" stroke={1} />
+                            Remover Destino
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="card-body">
+                <div className="row mb-3">
+
+                    <div className="col-12 col-lg mb-3 mb-lg-0">
+                        <label className="form-label required" htmlFor={`uf_${index}`}>Unidade Federativa:</label>
+                        <select
+                            className="form-select"
+                            disabled={importingFederativeUnits}
+                            value={destino.idUnidadeFederativa}
+                            onChange={(e) => {
+                                updateDestination(index, 'idUnidadeFederativa', e.target.value);
+                                updateDestination(index, 'idMunicipioDestino', null);
+                            }}
+                        >
+                            {importingFederativeUnits == false && destino.idUnidadeFederativa == null && <option selected disabled label="Selecionar Unidade Federativa..." />}
+                            {importingFederativeUnits && <option selected disabled label="Carregando..." />}
+                            {federativeUnits.map(federativeUnit =>
+                                <option
+                                    key={federativeUnit.idUnidadeFederativa}
+                                    value={federativeUnit.idUnidadeFederativa}
+                                    label={federativeUnit.NomeUnidadeFederativa}
+                                />
+                            )}
+                        </select>
+                    </div>
+
+                    <div className="col-12 col-lg mb-3 mb-lg-0">
+                        <label className="form-label required" htmlFor={`cidade_${index}`}>Cidade:</label>
+                        <select
+                            id={`cidade_${index}`}
+                            className="form-select"
+                            disabled={destino.idUnidadeFederativa == null || importingCities}
+                            value={destino.idMunicipioDestino}
+                            onChange={(e) => updateDestination(index, 'idMunicipioDestino', e.target.value)}
+                        >
+                            {importingCities == false && destino.idMunicipioDestino == null && <option selected disabled label="Selecionar Cidade..." />}
+                            {importingCities && <option selected disabled label="Carregando..." />}
+                            {cities.map(city =>
+                                <option
+                                    key={city.idMunicipio}
+                                    value={city.idMunicipio}
+                                    label={city.NomeMunicipio}
+                                />
+                            )}
+                        </select>
+                    </div>
+
+                    <div className="col-12 col-lg-3">
+                        <label className="form-label required">Data:</label>
+                        <DateInput value={destino.DataDestinoViagem} onChange={(DataDestinoViagem) => updateDestination(index, 'DataDestinoViagem', DataDestinoViagem)} />
+                    </div>
+                </div>
+
+                <div className="row mb-3">
+
+                </div>
+
+                <div className="row">
+                    <div className="col-12 mb-2">
+                        <h6 className="text-uppercase">Custos</h6>
+                    </div>
+                    <div className="col">
+                        <table className="table table-bordered">
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <IconTicket className="me-1" stroke={1} size={24} style={{ marginTop: "-2px" }} />
+                                        Passagem
+                                    </td>
+                                    <td style={{ width: "12rem" }}>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="R$ 0,00"
+                                            value={destino.custos?.find(_ => _.idTipoCusto == 1)?.ValorCustoDestino || ''}
+                                            onChange={(e) => {
+
+                                                updateCost(index, 1, e.target.value)
+                                            }}
+                                        />
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td>
+                                        <IconPizza className="me-1" stroke={1} size={24} style={{ marginTop: "-2px" }} />
+                                        Alimentação
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="R$ 0,00"
+                                            value={destino.custos?.find(_ => _.idTipoCusto == 2)?.ValorCustoDestino || ''}
+                                            onChange={(e) => updateCost(index, 2, e.target.value)}
+                                        />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <IconBed className="me-1" stroke={1} size={24} style={{ marginTop: "-2px" }} />
+                                        Hospedagem
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="R$ 0,00"
+                                            value={destino.custos?.find(_ => _.idTipoCusto == 3)?.ValorCustoDestino || ''}
+                                            onChange={(e) => updateCost(index, 3, e.target.value)}
+                                        />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <IconCar className="me-1" stroke={1} size={24} style={{ marginTop: "-2px" }} />
+                                        Locomoção
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="R$ 0,00"
+                                            value={destino.custos?.find(_ => _.idTipoCusto == 4)?.ValorCustoDestino || ''}
+                                            onChange={(e) => updateCost(index, 4, e.target.value)}
+                                        />
+                                    </td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ frontend/src/Pages/Public/SignUp.jsx
@@ -0,0 +1,203 @@
+import { useEffect, useState } from "react";
+import { USER } from "../../Models/User";
+import { IconPlane } from "@tabler/icons-react";
+import { useNavigate } from "react-router-dom";
+import { NewUser } from "../../Services/Users";
+import Sweetalert2 from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+const Swal = withReactContent(Sweetalert2);
+
+export default function SignUpPage() {
+
+    const [user, setUser] = useState(USER);
+    const navigate = useNavigate();
+
+    const [importing, setImporting] = useState(false);
+    const [processing, setProcessing] = useState(false);
+    const [errorOnProcess, setErrorOnProcess] = useState(false);
+
+    useEffect(() => {
+        // Adiciona a classe ao body quando o componente é montado
+        document.body.classList.add('bg-gradient');
+
+        // Remove a classe ao desmontar o componente
+        return () => {
+            document.body.classList.remove('bg-gradient');
+        };
+    }, []);
+
+    async function singUp() {
+
+        const validate = validateInputs();
+        if (!validate) return;
+
+        setProcessing(true);
+        setErrorOnProcess(null);
+
+        try {
+            const { nomeEmpregado, email, senha } = user;
+            await NewUser({ nomeEmpregado, email, senha, idCargo: 2, ativo: true });
+
+            Swal.fire({
+                title: 'Usuário cadastrado!',
+                text: "Seu usuário foi cadastrado com êxito.",
+                icon: 'success',
+                showCancelButton: false,
+                confirmButtonText: 'OK',
+            });
+
+            navigate("/login");
+
+
+        }
+        catch (error) {
+            setErrorOnProcess(error?.response?.data?.error || error.message);
+        }
+
+        setProcessing(false);
+    }
+
+    function validateInputs() {
+
+        if (!user.nomeEmpregado) {
+            setErrorOnProcess("Digite o seu nome.")
+            return false;
+        }
+
+        if (!user.email) {
+            setErrorOnProcess("Digite o seu e-mail.")
+            return false;
+        }
+
+        if (!user.senha) {
+            setErrorOnProcess("Digite o sua senha.")
+            return false;
+        }
+
+        if (user.confirmarSenha != user.senha) {
+            setErrorOnProcess("As senhas não conferem.")
+            return false;
+        }
+
+        return true;
+
+    }
+
+    return (
+        <>
+            <div className="container">
+                <div className="row d-flex justify-content-center align-items-center vh-100">
+                    <div className="col-auto">
+                        <div className="card shadow-lg" style={{ width: "30rem" }}>
+
+                            <div className="card-body">
+
+                                <h2 className="card-title text-center mb-4">
+                                    <IconPlane
+                                        size={58}
+                                        stroke={1.5}
+                                        className="me-2 text-primary"
+                                        style={{ transform: "rotate(-30deg)" }}
+                                    />
+                                    Gerenciador de Viagens
+                                </h2>
+
+
+                                <div className="mb-3">
+                                    <label className="form-label required" htmlFor="nameInput">Nome:</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="nameInput"
+                                        placeholder="Digite seu nome..."
+                                        required
+                                        disabled={processing || importing}
+                                        value={user.nomeEmpregado}
+                                        onChange={({ target }) => setUser(_ => ({ ..._, nomeEmpregado: target.value }))}
+                                    />
+                                </div>
+
+                                <div className="mb-3">
+                                    <label className="form-label required" htmlFor="emailInput">Email:</label>
+                                    <input
+                                        type="email"
+                                        className="form-control"
+                                        id="emailInput"
+                                        placeholder="Digite seu e-mail profissional..."
+                                        required
+                                        disabled={processing || importing}
+                                        value={user.email}
+                                        onChange={({ target }) => setUser(_ => ({ ..._, email: target.value }))}
+                                    />
+                                </div>
+
+                                <div className="mb-3">
+                                    <label className="form-label required" htmlFor="passwordInput">Senha:</label>
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        id="passwordInput"
+                                        placeholder="Crie uma senha..."
+                                        required
+                                        disabled={processing || importing}
+                                        value={user.senha}
+                                        onChange={({ target }) => setUser(_ => ({ ..._, senha: target.value }))}
+                                    />
+                                </div>
+
+                                <div className="mb-3">
+                                    <label className="form-label required" htmlFor="passwordInput">Confirmar Senha:</label>
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        id="passwordInput"
+                                        placeholder="Confirme sua senha..."
+                                        required
+                                        disabled={processing || importing}
+                                        value={user.confirmarSenha}
+                                        onChange={({ target }) => setUser(_ => ({ ..._, confirmarSenha: target.value }))}
+                                    />
+                                </div>
+
+
+                                {errorOnProcess && (
+                                    <div className="my-4 text-center text-bg-danger py-2 rounded-3">
+                                        <p className="p-0 m-0">{errorOnProcess}</p>
+                                    </div>
+                                )}
+
+
+                                <div className="d-grid gap-2">
+
+                                    <button
+                                        type="button"
+                                        className="btn btn-primary"
+                                        disabled={processing}
+                                        onClick={singUp}
+                                    >
+                                        {processing && <span className="spinner-border spinner-border-sm me-2" aria-hidden="true" />}
+                                        {processing ? "Cadastrando..." : "Cadastrar"}
+                                    </button>
+
+                                    <button
+                                        type="button"
+                                        className="btn btn-secondary"
+                                        disabled={processing}
+                                        onClick={() => navigate("/login")}
+                                    >
+                                        Voltar
+                                    </button>
+
+                                </div>
+
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+
+}
